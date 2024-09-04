0e061aebd1d57958c6e8e4199b4713cdcda21c5a
Rayana0 <rayoona890@gmail.com>
2024-09-04 11:42:32 +0300

    second upload

---
+++ lib/homescreen.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:student_tawsel_app/chatscreen.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(22),
+          bottomRight: Radius.circular(22),
+          topLeft: Radius.circular(0),
+          topRight: Radius.circular(0),
+        )),
+        backgroundColor: const Color(0xff182243),
+        toolbarHeight: 139,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.grey[300],
+              backgroundImage: const AssetImage("assets/boy.png"),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Mohammed",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Mohammed@gmail.com",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(
+              Icons.settings,
+              size: 40,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child: Image.asset("assets/Rect.png")),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "Latest Notices",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 380,
+              child: ListView.builder(
+                  itemCount: 4,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 91,
+                      width: 353,
+                      child: Card(
+                        child: ListTile(
+                          title: const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                " MR :  Adel",
+                                style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16,
+                                    color: Colors.black),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                " Science Teacher",
+                                style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                    color: Colors.black),
+                              ),
+                              Text(
+                                "Ahmed",
+                                style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          trailing: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) {
+                                    return ChatScreen();
+                                  }));
+                                },
+                                child: const Text(
+                                  " Message",
+                                  style: TextStyle(
+                                      fontFamily: "Poppins",
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                      color: Colors.black,
+                                      decoration: TextDecoration.underline),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                " Management Education Serves ",
+                                style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 8,
+                                    color: Colors.black),
+                              ),
+                              const Text(
+                                " And Buses At Your Home",
+                                style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 8,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_tawsel_app/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,120 +15,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffF6F6F6)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffF6F6F6)),
         useMaterial3: true,
       ),
-      home: firstScreen(),
-    );
-  }
-}
-
-class firstScreen extends StatelessWidget {
-  const firstScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xff182243),
-        title: Text(
-          "Chat",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        toolbarHeight: 139,
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
-          buildChatItem(
-              'assets/avatar.png', 'MR : Adel', 'Science Teacher', '25 Mar'),
-          buildChatItem(
-              'assets/avatar.png', 'MR : Jasem', 'Math Teacher', '24 Mar'),
-          buildChatItem(
-              'assets/avatar.png', 'MR : Ahmed', 'Science Teacher', '24 Mar'),
-        ],
-      ),
-    );
-  }
-
-  Widget buildChatItem(
-      String avatarPath, String name, String profession, String date) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 16.0),
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Color(0XFFF6F6F6),
-        borderRadius: BorderRadius.circular(16.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 10,
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Column(
-            children: [
-              CircleAvatar(
-                radius: 30.0,
-                backgroundImage: AssetImage(avatarPath),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                name,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(width: 16.0),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  profession,
-                  style: TextStyle(
-                    //color:Color(0fxxD9D9D9),
-                    fontSize: 16.0,
-                  ),
-                ),
-                SizedBox(height: 40.0),
-                Text(
-                  date,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      home: const HomePage(),
     );
   }
 }
